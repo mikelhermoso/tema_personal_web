2147f740b35f7783c699a879ea10d4e3b122ab3f
mikel <mikel@creative>
2017-05-12 09:51:50 +0200

    12/05/2017 - Plantilla web personal

---
+++ layouts/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="contentTextoFooter">  
+        <p class="textoFooter">&copy <?php echo $copy." ". $año." ".$copyAutor ?></p>
+    </div>
+</footer>
---
+++ layouts/redesSociales.php
@@ -0,0 +1,17 @@
+<div id="contentRedesSociales">
+    <div id="containerRedesSociales">
+        <h1 class="titRedes"><?php echo $sigueme ?></h1>
+        <div id="contentRedesIn">
+            <a href="https://twitter.com"><h3 class="titRedesIn"><span class="colorLinkedin"><?php echo $linkedin ?></span></h3>
+            <img class="imgRedesIn" src="img/linkedin.png" alt="linkedin" /></a>
+        </div>
+        <div id="contentRedesIn">
+            <a href="https://www.youtube.com"><h3 class="titRedesIn"><span class="colorYoutube"><?php echo $youtube ?></span></h3>
+            <img class="imgRedesIn" src="img/youtube.png" alt="youtube" /></a>
+        </div>
+        <div id="contentRedesIn">
+            <a href="https://www.linkedin.com"><h3 class="titRedesIn"><span class="colorTwitter"><?php echo $twitter ?></span></h3>
+            <img class="imgRedesIn" src="img/twitter.png" alt="twitter" /></a>
+        </div>
+    </div>
+</div>
